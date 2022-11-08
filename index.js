1453a1e24f2509de10b524f6109fe4db1d308fd9
shourovr82 <shafinur512@gmail.com>
2022-11-08 16:16:40 +0600

    added my reviews from mongodb to client side

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ServerApiVersion, MongoClient, ObjectId } = require('mongodb');
+const { query } = require('express');
 require('dotenv').config();
-console.log(process.env)
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,6 +14,75 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ikwqeh8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+  try {
+    const serviceCollection = client.db('travetorWorld').collection('services');
+    const reviewsCollection = client.db('travetorWorld').collection('reviews')
+
+
+    // get services data
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    // get Service Details 
+    app.get('/serviceDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service)
+    })
+
+    // get customer reviews
+
+    app.get('/reviews', async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews)
+    })
+
+
+
+    app.get('/myreview', async (req, res) => {
+      const useremail = req.query?.email;
+      let query = {};
+      if (req.query?.email) {
+        query = {
+          email: useremail
+        }
+        console.log(query);
+      }
+
+
+      const cursor = reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      console.log(reviews);
+      res.send(reviews)
+
+
+    })
+
+    // post customer reviews
+    app.post('/postReview', async (req, res) => {
+      const review = req.body;
+      console.log(review);
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result)
+    })
+
+
+
+
+  }
+  finally { }
+}
+
+run().catch(err => console.log(err))
+
 
 
 app.get('/', (req, res) => {
@@ -21,7 +90,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log('Travetor server is running on ', uri);
+  console.log('Travetor server is running on ', port);
 })
 
 
